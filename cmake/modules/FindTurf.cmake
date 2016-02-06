4249e1abd767b5cbbe9b722710b90ed49cd0390a
Jeff Preshing <filter-github@preshing.com>
2016-02-06 11:51:52 -0500

    Make Junction installable

--- cmake/modules/FindTurf.cmake
+++ cmake/modules/FindTurf.cmake
@@ -1,25 +1,30 @@
 #----------------------------------------------
-# When Turf is found, it's basically as if TurfProjectDefs.cmake was included.
+# This find module locates the Turf *source*.
 #
-# Later, this might find binary packages too.
+# Later, it might find binary packages too.
 #
 # Sets:
 #    TURF_FOUND
-#    TURF_ROOT - the root folder
+#    TURF_ROOT - the root folder, containing CMakeLists.txt
 #    TURF_INCLUDE_DIRS - pass to include_directories()
-#    AddTurfTarget() - call this to actually add the target
-#
-# You'll want to set the compiler options before calling AddTurfTarget().
 #----------------------------------------------
 
-find_path(TURF_ROOT "turf/Core.h" PATHS
-    "${CMAKE_CURRENT_SOURCE_DIR}/../turf"
-    "${CMAKE_SOURCE_DIR}/../turf"
-    "${CMAKE_CURRENT_LIST_DIR}/../../../turf")
-
 if(TURF_ROOT)
-    set(TURF_FOUND TRUE)
+    get_filename_component(fullPath "${TURF_ROOT}" ABSOLUTE)
+    if(EXISTS "${fullPath}/cmake/turf_config.h.in")
+        set(TURF_FOUND TRUE)
+    endif()
 else()
+    find_path(TURF_ROOT "cmake/turf_config.h.in" PATHS
+        "${CMAKE_CURRENT_SOURCE_DIR}/../turf"
+        "${CMAKE_SOURCE_DIR}/../turf"
+        "${CMAKE_CURRENT_LIST_DIR}/../../../turf")
+    if(TURF_ROOT)
+        set(TURF_FOUND TRUE)
+    endif()
+endif()
+
+if(NOT TURF_FOUND)
     message("Can't find Turf!")
     if(Turf_FIND_REQUIRED)
         message(FATAL_ERROR "Missing required package Turf")
